e6368c81cd0435cd3b07d1e570537bea2dedb97c
Wulfazz <145058888+Wulfazz@users.noreply.github.com>
2024-02-15 16:08:50 +0100

    Add files via upload

---
+++ config/php/index.php
@@ -0,0 +1,39 @@
+<?php
+ob_start(); // Active la sortie tamponnée
+session_start();
+?>
+
+    <!-- connexion à la bdd -->
+    <?php
+    include 'db.php';
+    ?>
+
+    <?php
+    include 'header.php';
+    ?>
+
+    <?php
+    include 'navbar.php';
+    ?>
+
+        <!-- Contenus site (les différentes pages) -->
+
+        <?php
+        // Permet de reconnaître la page sur laquelle on se trouve, si la page est l'une des pages php, la page se chargera. 
+        if (isset($_GET['page'])){
+            $page = $_GET['page'];
+            include "pages/$page.php";
+        }
+        ?>
+
+        <?php
+        if (isset($_GET['exo'])){
+            $exo = $_GET['exo'];
+            include "exo/$exo.php";
+        }
+        ?>
+
+    <!-- footer -->
+    <?php
+    include 'footer.php';
+    ?>
