b3e6fa215fc3136b8ac2b8c4e87b1a7b9ab38764
Wulfazz <teito.klein@laposte.net>
2024-02-15 16:18:48 +0100

    Update

--- config/php/php/navbar.php
+++
@@ -1,9 +0,0 @@
-<!-- navbar pour changer de page (pas besoin d'avoir plusieurs pages) -->
-<header>
-    <nav>
-        <ul>
-            <li><a href="?exo=poo">Exo</a></li>
-            <li><a href="?exo=5vs5">Tour par tour</a></li>
-        </ul>
-    </nav>
-</header>
