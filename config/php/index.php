eb4415ea03cc20bd42601999813675bfc54adf12
Wulfazz <teito.klein@laposte.net>
2024-02-19 13:42:50 +0100

    update

--- config/php/index.php
+++ config/php/index.php
@@ -1,30 +1,39 @@
 <?php
-ob_start(); // Active la sortie tamponnée
+require_once 'classes/personnage.php';
+require_once 'classes/combattant.php';
+require_once 'classes/mage.php';
+require_once 'classes/support.php';
+require_once 'classes/tireur.php';
+
+if (isset($_GET['reset'])) {
+    ob_start(); // Démarrez la sortie tamponnée
+    echo "Ceci est une sortie avant header()";
+    header("Location: index.php"); // Maintenant, cela fonctionne correctement
+    exit();
+    ob_end_flush(); // Envoyez le contenu du tampon au navigateur
+}
+
 session_start();
 ?>
 
     <!-- connexion à la bdd -->
     <?php
-    include 'db.php';
+    require_once 'db.php';
     ?>
 
     <?php
-    include 'header.php';
+    include  'header.php';
     ?>
 
     <?php
-    include 'navbar.php';
+    include  'navbar.php';
     ?>
 
     <?php
-    include 'exo/body.php';
-    ?>
-
-    <?php
-    include 'exo/persos.php';
+    include  'body.php';
     ?>
 
     <!-- footer -->
     <?php
-    include 'footer.php';
+    include  'footer.php';
     ?>
